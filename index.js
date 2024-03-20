10be73160fab9eaeac25f21abcab62992bf432d9
Kyle <kylejrgurlad@gmail.com>
2024-03-20 11:14:08 +0300

    completed task

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const locationOfHQ = 42
+
+function distanceFromHqInBlocks(distanceInBlocks) {
+    if (distanceInBlocks > 42) {
+       return distanceInBlocks-locationOfHQ
+    } else if ( distanceInBlocks < 42 )
+    return locationOfHQ - distanceInBlocks
+}
+function distanceFromHqInFeet (distanceInBlocks){
+    if (distanceInBlocks > 42) {
+        return ( distanceInBlocks- locationOfHQ) * 264
+    } else if (distanceInBlocks < 42) {
+        return ( locationOfHQ - distanceInBlocks) * 264
+    }
+}
+function distanceTravelledInFeet(locationOfHQ,distanceInBlocks){
+    if(distanceInBlocks > 42){
+        return (distanceInBlocks-locationOfHQ) * 264
+    } else if (distanceInBlocks < 42) {
+        return ( locationOfHQ-distanceInBlocks) * 264
+    }
+
+}
+function calculatesFarePrice(locationOfHQ,distanceInBlocks) {
+    const distance = distanceTravelledInFeet(locationOfHQ, distanceInBlocks)
+    if ((distance) <= 400) {
+        return 0
+    } else if ((distance) <= 2000) {
+        return 2.56
+    } else if ( distance <=2500 ) {
+        return 25
+    } else if ( distance > 2500){
+        return 'cannot travel that far'
+    }
+
+}
+
+
